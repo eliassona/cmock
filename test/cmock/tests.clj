f2b0043924021cd26247095b0e5de46cbe7be5f6
Anders Eliasson <anders.eliasson@digitalroute.com>
2013-08-16 08:46:13 +0200

    more tests

--- test/cmock/tests.clj
+++ test/cmock/tests.clj
@@ -1,6 +1,9 @@
 (ns cmock.tests
   (:use [cmock.core]
-        [clojure.test]))
+        [clojure.test])
+  (:import (org.mockito Mockito)
+           (java.io File))
+  )
 
 
 
@@ -11,3 +14,19 @@
                      (exists) => true
                      (getPath) => "path")))
       ))
+
+
+
+(deftest actual-mocking
+  (let [f (Mockito/mock File)]
+    (is (not (.exists f)))
+    (is (= nil (.getPath f)))
+    (is (not (.setReadable f false true)))
+    (provided f
+              (exists) => true
+              (getPath) => "path"
+              (setReadable false true) => true)
+
+    (is (.exists f))
+    (is (= "path" (.getPath f)))
+    (is (.setReadable f false true))))
